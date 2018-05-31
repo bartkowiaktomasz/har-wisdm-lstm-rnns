942de7710fec0b9da8979b3ea3658bb1b86db8a5
Tomasz  Bartkowiak <tomasz.bartkowiak17@imperial.ac.uk>
2018-05-31 23:27:58 +0100

    matplotlib Agg

--- HAR_Recognition.py
+++ HAR_Recognition.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import tensorflow as tf
 import seaborn as sns
 from scipy import stats
@@ -9,6 +8,10 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 
 import pickle
+
+import matplotlib
+matplotlib.use('Agg')   # Use Agg backend to save figures
+import matplotlib.pyplot as plt
 
 ##################################################
 ### GLOBAL VARIABLES
@@ -22,7 +25,18 @@ COLUMN_NAMES = [
     'z-axis'
 ]
 
-RANDOM_SEED = 42
+LABELS = [
+    'Downstairs',
+    'Jogging',
+    'Sitting',
+    'Standing',
+    'Upstairs',
+    'Walking'
+]
+
+DATA_PATH = 'data/WISDM_ar_v1.1_raw.txt'
+
+RANDOM_SEED = 13
 
 # Data preprocessing
 SEGMENT_TIME_SIZE = 200
@@ -30,22 +44,24 @@ TIME_STEP = 200
 
 # Model
 N_CLASSES = 6
-N_LSTM_LAYERS = 2
-N_HIDDEN_NEURONS = 32
 N_FEATURES = 3  # x-acceleration, y-acceleration, z-acceleration
 
 # Hyperparameters
+N_LSTM_LAYERS = 2
+N_HIDDEN_NEURONS = 32
+N_EPOCHS = 50
+BATCH_SIZE = 64
 L2_LOSS = 0.0015
 LEARNING_RATE = 0.0025
-EPOCHS_COUNT = 20
-BATCH_SIZE = 64
 
 ##################################################
 ### FUNCTIONS
 ##################################################
 
 # Returns a tenforflow LSTM NN
-def createLSTM(input):
+# Input of shape (BATCH_SIZE, SEGMENT_TIME_SIZE, N_FEATURES)
+def createLSTM(X):
+
     W = {
         'hidden': tf.Variable(tf.random_normal([N_FEATURES, N_HIDDEN_NEURONS])),
         'output': tf.Variable(tf.random_normal([N_HIDDEN_NEURONS, N_CLASSES]))
@@ -56,7 +72,8 @@ def createLSTM(input):
         'output': tf.Variable(tf.Variable(tf.random_normal([N_CLASSES])))
     }
 
-    X = tf.transpose(input, [1, 0, 2])
+    # Transpose and then reshape to 2D of size (BATCH_SIZE * SEGMENT_TIME_SIZE, N_FEATURES)
+    X = tf.transpose(X, [1, 0, 2])
     X = tf.reshape(X, [-1, N_FEATURES])
     hidden = tf.nn.relu(tf.matmul(X, W['hidden']) + b['hidden'])
     hidden = tf.split(hidden, SEGMENT_TIME_SIZE, 0)
@@ -68,10 +85,10 @@ def createLSTM(input):
 
     outputs, _ = tf.contrib.rnn.static_rnn(lstm_layers, hidden, dtype=tf.float32)
 
-    # Get output for the last time step
-    lstm_last_output = outputs[-1]
+    # Get output for the last time step from a "many to one" architecture
+    last_output = outputs[-1]
 
-    return tf.matmul(lstm_last_output, W['output'] + b['output'])
+    return tf.matmul(last_output, W['output'] + b['output'])
 
 ##################################################
 ### MAIN
@@ -79,12 +96,13 @@ def createLSTM(input):
 if __name__ == '__main__':
 
     # LOAD DATA
-    data = pd.read_csv('data/WISDM_ar_v1.1_raw.txt', header=None, names=COLUMN_NAMES)
+    data = pd.read_csv(DATA_PATH, header=None, names=COLUMN_NAMES)
     data['z-axis'].replace({';': ''}, regex=True, inplace=True)
     data = data.dropna()
 
     # SHOW ACTIVITY GRAPH
-    # data['activity'].value_counts().plot(kind='bar', title='Activity type')
+    activity_type = data['activity'].value_counts().plot(kind='bar', title='Activity type')
+    activity_type.savefig('activity_type.png')
 
     # DATA PREPROCESSING
     data_convoluted = []
@@ -120,11 +138,11 @@ if __name__ == '__main__':
 
     ##### BUILD A MODEL
     # Placeholders
-    X = tf.placeholder(tf.float32, [None, SEGMENT_TIME_SIZE, N_FEATURES], name="input")
+    X = tf.placeholder(tf.float32, [None, SEGMENT_TIME_SIZE, N_FEATURES], name="X")
     y = tf.placeholder(tf.float32, [None, N_CLASSES])
 
     y_pred = createLSTM(X)
-    pred_softmax = tf.nn.softmax(y_pred)
+    y_pred_softmax = tf.nn.softmax(y_pred)
 
     # LOSS
     l2 = L2_LOSS * sum(tf.nn.l2_loss(i) for i in tf.trainable_variables())
@@ -132,7 +150,7 @@ if __name__ == '__main__':
 
     # OPTIMIZER
     optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
-    correct_pred = tf.equal(tf.argmax(pred_softmax, 1), tf.argmax(y, 1))
+    correct_pred = tf.equal(tf.argmax(y_pred_softmax, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
 
     # TRAINING
@@ -144,12 +162,12 @@ if __name__ == '__main__':
 
     train_count = len(X_train)
 
-    for i in range(1, EPOCHS_COUNT + 1):
+    for i in range(1, N_EPOCHS + 1):
         for start, end in zip(range(0, train_count, BATCH_SIZE), range(BATCH_SIZE, train_count + 1, BATCH_SIZE)):
             sess.run(optimizer, feed_dict={X: X_train[start:end], y: y_train[start:end]})
 
-        _, acc_train, loss_train = sess.run([pred_softmax, accuracy, loss], feed_dict={X: X_train, y: y_train})
-        _, acc_test, loss_test = sess.run([pred_softmax, accuracy, loss], feed_dict={X: X_test, y: y_test})
+        _, acc_train, loss_train = sess.run([y_pred_softmax, accuracy, loss], feed_dict={X: X_train, y: y_train})
+        _, acc_test, loss_test = sess.run([y_pred_softmax, accuracy, loss], feed_dict={X: X_test, y: y_test})
 
         history['train_loss'].append(loss_train)
         history['train_acc'].append(acc_train)
@@ -161,11 +179,11 @@ if __name__ == '__main__':
 
         print(f'epoch: {i} test accuracy: {acc_test} loss: {loss_test}')
 
-    predictions, acc_final, loss_final = sess.run([pred_softmax, accuracy, loss], feed_dict={X: X_test, y: y_test})
+    predictions, acc_final, loss_final = sess.run([y_pred_softmax, accuracy, loss], feed_dict={X: X_test, y: y_test})
     print(f'final results: accuracy: {acc_final} loss: {loss_final}')
 
     # PLOT GRAPHS
-    plt.figure(figsize=(12, 8))
+    error_plot = plt.figure(figsize=(12, 8))
 
     plt.plot(np.array(history['train_loss']), "r--", label="Train loss")
     plt.plot(np.array(history['train_acc']), "g--", label="Train accuracy")
@@ -178,20 +196,19 @@ if __name__ == '__main__':
     plt.ylabel('Training Progress (Loss or Accuracy values)')
     plt.xlabel('Training Epoch')
     plt.ylim(0)
-
+    error_plot.savefig('error_plot.png')
     #plt.show()
 
 
     # CONFUSION MATRIX
-    LABELS = ['Downstairs', 'Jogging', 'Sitting', 'Standing', 'Upstairs', 'Walking']
-
     max_test = np.argmax(y_test, axis=1)
     max_predictions = np.argmax(predictions, axis=1)
     confusion_matrix = metrics.confusion_matrix(max_test, max_predictions)
 
-    plt.figure(figsize=(16, 14))
+    confusion_matrix = plt.figure(figsize=(16, 14))
     sns.heatmap(confusion_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d");
     plt.title("Confusion matrix")
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+    confusion_matrix.savefig('confusion_matrix.png')
     #plt.show();
